1f0ce7732030f6b997eab729e6c28ec3e8ab738a
Mukul Kumar <mukul0000kumar@gmail.com>
2022-07-23 01:21:49 +0530

    auto increment v fix 1

--- script.groovy
+++ script.groovy
@@ -2,9 +2,9 @@ def incrementVersion() {
     sh 'mvn build-helper:parse-version versions:set \
         -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
         versions:commit'
-        def matcher = readFile('pom.xml').matcher('<version>(.+)</version>')
-        def version = matcher[0][1]
-        env.IMAGE_NAME = "$version-$BUILD_NUMBER"
+    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+    def version = matcher[0][1]
+    env.IMAGE_NAME = "$version-$BUILD_NUMBER"
 }
 
 def makeJar() {
